8e91f79eaf98f8c92c4dde95a67ede38998c071d
DanilK841 <59386623+DanilK841@users.noreply.github.com>
2024-10-04 18:48:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    double fib1 = 0.0;
+    double fib2 = 1.0;
+    double fib3 = 1.0;
+    double fib4 = 2.0;
+    double fib5 = 3.0;
+    
+    std::cout<<"Fib number 1: "<<fib1<<std::endl;
+    std::cout<<"Fib number 2: "<<fib2<<std::endl;
+    std::cout<<"Fib number 3: "<<fib3<<std::endl;
+    std::cout<<"Fib number 4: "<<fib4<<std::endl;
+    std::cout<<"Fib number 5: "<<fib5<<std::endl;
+}
